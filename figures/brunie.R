d7c4140e6c52e9f8766f6a0a5ec3d98aefce9c28
Dennis Evangelista <devangel77b@gmail.com>
2026-02-23 10:04:03 -0500

    blah

--- figures/brunie.R
+++ figures/brunie.R
@@ -9,15 +9,16 @@ data <- tibble(raw)
 ypred <- function(t){
   -0.5*9.81*t^2+5
 }
-fig1 <- ggplot(data) +
-    geom_point(aes(x=t_s,y=y_m,shape=type))+
-    geom_function(fun=ypred,color="blue")+
+fig1 <- ggplot(data,aes(x=t_s,y=y_m,color=type)) +
+    geom_point()+
+    geom_smooth(method="lm",formula=y~I(x^2),se=FALSE)+
     xlab('$t$, \\unit{\\second}')+
     ylab('$y$, \\unit{\\meter}')+
     theme_bw(base_size=8)+
     theme(legend.position="inside",
 	legend.position.inside=c(0.95,0.95),
 	legend.justification.inside=c("right","top"),
+	legend.key.size=unit(4,"pt"),
 	legend.title=element_blank())
 ggsave('fig1.svg',plot=fig1,width=3.4167,height=2,units='in')
 
@@ -28,15 +29,16 @@ vdata <- tibble(read.csv('velocity.csv',header=TRUE))
 vpred <- function(t){
   -9.81*t
 }
-fig2 <- ggplot(vdata) +
-    geom_point(aes(x=t_s,y=v_ms,shape=type))+
-    geom_function(fun=vpred,color="blue")+
+fig2 <- ggplot(vdata,aes(x=t_s,y=v_ms,color=type)) +
+    geom_point()+
+    geom_smooth(method="lm",formula=y~x,se=FALSE)+
     xlab('$t$, \\unit{\\second}')+
     ylab('$v_y$, \\unit{\\meter\\per\\second}')+
     theme_bw(base_size=8)+
     theme(legend.position="inside",
 	legend.position.inside=c(0.95,0.95),
 	legend.justification.inside=c("right","top"),
+	legend.key.size=unit(4,"pt"),
 	legend.title=element_blank())
 ggsave('fig2.svg',plot=fig2,width=3.4167,height=2,units='in')
 
